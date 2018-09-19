a89d36f8e87e7955e0ad8346fe2c8be33479db9f
Langenfelds, Arturs <a.langenfelds@a1qa.com>
2018-09-19 21:40:46 +0300

    first commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({
+                    posts: response.data
+                })
+                console.log(response.data);
+            })
+    }
+
+    render () {
+        return (
+            <div>
+                <section className="Posts">
+                {this.state.posts.map( (post) => (
+                    <Post post={post}></Post>
+                ))}
+                    {/* <Post />
+                    <Post />
+                    <Post /> */}
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
